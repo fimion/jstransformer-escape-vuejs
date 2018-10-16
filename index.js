5b7b6d28f24cda9b36561e4c4c81eeee24b00b58
Alex Riviere <fimion@gmail.com>
2018-10-16 10:26:46 -0400

    update testing

--- index.js
+++ index.js
@@ -6,34 +6,32 @@ exports.name = 'escape-vuejs'
 exports.outputFormat = 'html'
 
 exports.render = function (str) {
-  let result = escapeHtml(str),
-      match = /[{}]/gm.exec(result);
-  if(!match){
-    return result;
+  const result = escapeHtml(str)
+  const match = /[{}]/gm.exec(result)
+  if (!match) {
+    return result
   }
-  let escape,
-    html = '',
-    index = 0,
-    lastIndex = 0;
+  let escape
+  let html = ''
+  let index = 0
+  let lastIndex = 0
 
-  for(index=match.index; index < result.length; index++){
-    switch(result.charCodeAt(index)){
+  for (index = match.index; index < result.length; index++) {
+    switch (result.charCodeAt(index)) {
       case 123: // {
-        escape = '&#123;';
-        break;
+        escape = '&#123;'
+        break
       case 125: // }
-        escape = '&#125;';
-        break;
+        escape = '&#125;'
+        break
       default:
-        continue;
+        continue
     }
-    if(lastIndex !== index){
-      html += result.substring(lastIndex, index);
+    if (lastIndex !== index) {
+      html += result.substring(lastIndex, index)
     }
     lastIndex = index + 1
-    html += escape;
+    html += escape
   }
-  return lastIndex !== index
-    ? html + result.substring(lastIndex, index)
-    : html;
+  return (lastIndex === index) ? html : html + result.substring(lastIndex, index)
 }
