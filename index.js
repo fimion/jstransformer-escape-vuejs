073c9cc9f87ff594ddabda8b46d656b8064ce773
Alex Riviere <fimion@gmail.com>
2018-10-16 07:15:16 -0400

    add our escaping

--- index.js
+++ index.js
@@ -2,9 +2,38 @@
 
 const escape = require('escape-html')
 
-exports.name = 'escape-html'
+exports.name = 'escape-vuejs'
 exports.outputFormat = 'html'
 
 exports.render = function (str) {
-  return escape(str)
+  let result = escape(str),
+      match = /[{}]/.exec(result);
+  if(!match){
+    return result;
+  }
+  let escape,
+    html = '',
+    index = 0,
+    lastIndex = 0;
+
+  for(index=match.index; index < result.length; index++){
+    switch(result.charCodeAt(index)){
+      case 123: // {
+        escape = '&#123;';
+        break;
+      case 125: // }
+        escape = '&#125;';
+        break;
+      default:
+        continue;
+    }
+    if(lastIndex !== index){
+      html += result.substring(lastIndex, index);
+    }
+    lastIndex = index + 1
+    html += escape;
+  }
+  return lastIndex !== index
+    ? html + result.substring(lastIndex, index)
+    : html;
 }
